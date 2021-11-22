f1d5dfdf645f5cf92786384b2d85e3327a1e1556
ldjmoreira <ldjmoreira@hotmail.com>
2021-11-22 15:52:00 -0300

    first

---
+++ saudacaoMid.js
@@ -0,0 +1,10 @@
+  
+function saudacao(nome) {
+    return function(req, res, next) {
+        console.log(`Seja bem vindo ${nome}.`)
+        
+        next()
+    }
+}
+
+module.exports =  saudacao
